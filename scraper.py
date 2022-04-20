26632decc17c960a643796f2ba2b0b6c71425705
Alexis Chau <79122383+alexischau@users.noreply.github.com>
2022-04-19 19:33:41 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,10 +1,27 @@
 import re
-from urllib.parse import urlparse, urljoin
+import nltk
+import time
+nltk.download('punkt')
+from urllib.parse import urlparse, urljoin, urldefrag
+from urllib import robotparser
 from bs4 import BeautifulSoup
+from collections import defaultdict
+
+VISITED = set()
+LONGEST_PAGE_COUNT = -1
+LONGEST_PAGE = ""
+WORDS_COUNT = defaultdict(int)
+
+STOPWORDS = nltk.corpus.stopwords.words('english')
+STOPWORDS.append({"we've", 'cannot', "they're", "they've", "let's", "where's", "when's", 'could', "he's", "here's", "who's", "we'd", "he'll", "why's", 'ought', "she'd", "i'll", 'would', "i'm", "i've", "that's", "we'll", "can't", "they'll", "they'd", "he'd", "we're", "what's", "how's", "there's", "i'd", "she'll"})
+# stopwords is NLTK's + the ones in the first list that the professor gave
+
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     # count words and keep track of frequency of words
+    with open('count.txt', "w") as f:
+        print(WORDS_COUNT, file=f)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
@@ -18,24 +35,50 @@ def extract_next_links(url, resp):
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
 
-
+    VISITED.add(url)
+    # print('VISITED: ', VISITED)
+    new_urls = []
     if resp.status == 200:
-        if resp.raw_response.content:
-            with open('output.txt', "w") as file:
-                soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
-                # print(soup.prettify())
-                for link in soup.find_all('a'):
-                    final = urljoin(url, link.get('href'))
-                    print(type(final))
-                    print('******************************************************\n')
-                    file.write(final)
-                    #file.write(str(link))
-                    file.write('\n')
-                
-            # check hyperlinks and scrape them
+        # use robot.txt to see if we are allowed to crawl
+        rp = robotparser.RobotFileParser()
+        # slice url to get the root page, and append '/robots.txt'
+        # ex: 'https://google.com/foo/bar' -> 'https://google.com/robot.txt
+        root_page = urlparse(url).scheme + '://' + urlparse(url).netloc + '/robots.txt'
+        rp.set_url(root_page)
+        rp.read()
+        if rp.can_fetch("*", url):
+            # politeness
+            time.sleep(rp.crawl_delay("*"))
+            if resp.raw_response.content:
+                with open('output.txt', "w") as file:
+                    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+                    # tokenize text here
+                    text = soup.get_text()
+                    tokens = nltk.word_tokenize(text)
+                    # print(soup.prettify())
+                    for link in soup.find_all('a'):
+                        final = urldefrag(urljoin(url, link.get('href')).rstrip('/'))[0]
+
+                        file.write(final)
+                        file.write('\n')
+
+                        if final not in VISITED and final not in new_urls:
+                            new_urls.append(final)
+                            # process tokenize using nltk, add to defaultdict
+                    if len(tokens) > LONGEST_PAGE_COUNT:
+                        LONGEST_PAGE_COUNT = len(tokens)
+                        LONGEST_PAGE = url
+                    for word in tokens:
+                        word_lower = word.lower()
+                        if word_lower not in STOPWORDS:
+                            WORDS_COUNT[word_lower] += 1
+
+                #print('[SCRAPER] new_urls: ', new_urls)
+
+                # check hyperlinks and scrape them
     else:
-        print("An error occurred: ", resp.error)
-    return list()
+        print("An error occurred: ", resp.status)
+    return new_urls
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
